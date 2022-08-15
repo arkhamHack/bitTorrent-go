b864f3fe530452f6bc5ae78af5852d6291119051
Avigyan Sinha <72064090+arkhamHack@users.noreply.github.com>
2022-08-15 11:53:58 +0530

    Update peers.go

--- peers/peers.go
+++ peers/peers.go
@@ -15,15 +15,15 @@ type Peer struct {
 func UnmarshalPeer(peerList []byte) ([]Peer, error) {
 	const peerSize = 6
 	peerNum := len(peerList) / peerSize
-	if (peerNum)%peerSize != 0 {
-		err := fmt.Errorf("Peers are malfunctioning")
+	if len(peerList)%peerSize != 0 {
+		err := fmt.Errorf("peers are malfunctioning")
 		return nil, err
 	}
 	peers := make([]Peer, peerNum)
 	for i := 0; i < peerNum; i++ {
 		offSet := i * peerSize
 		peers[i].Ip = net.IP(peerList[offSet : offSet+4])
-		peers[i].Port = binary.BigEndian.Uint16(peerList[offSet+4 : offSet+6])
+		peers[i].Port = binary.BigEndian.Uint16([]byte(peerList[offSet+4 : offSet+6]))
 
 	}
 	return peers, nil
